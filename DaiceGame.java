523755347fd4d2a80bc0ab366dfb8a669db57b8a
Eyuki0705 <s1320260@u-aizu.ac.jp>
2025-07-03 16:05:53 +0900

    Greeting added

--- DaiceGame.java
+++ DaiceGame.java
@@ -1,7 +1,14 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class DiceGame {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What is your name?");
+        String name = scanner.nextLine();
+        System.out.println("Hello,"+ name + "!");
+        
         System.out.println("Rolling dice...");
         Random random = new Random();
         int die1 = random.nextInt(6) + 1; 
